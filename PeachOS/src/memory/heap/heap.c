f99bb9ea399d8bad15c2bc224590606ccac78ce6
Robert Smith <robertets88@gmail.com>
2026-06-30 15:57:51 -0400

    prototype and makefile for heap

---
+++ PeachOS/src/memory/heap/heap.c
@@ -0,0 +1,17 @@
+#include "heap.h"
+/*
+    Create heap.
+    If return 0, okay.
+    If return below 0, error.
+        - The negative value represents error code.
+
+    Args: Accept pointer to heap.
+          Accept pointer to heap data pool.
+          Accept pointer to end of heap.
+          Accept heap table.
+*/
+int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* table)
+{
+    return 0;
+    
+}
