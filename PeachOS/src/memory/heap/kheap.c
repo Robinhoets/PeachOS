713b5d710c0eda148bde27f3086f0706084e2a92
Robert Smith <robertets88@gmail.com>
2026-07-01 11:58:58 -0400

    skeleton kheap

---
+++ PeachOS/src/memory/heap/kheap.c
@@ -0,0 +1,25 @@
+#include "kheap.h"
+#include "heap.h"
+#include "config.h"
+#include "kernel.h"
+
+struct heap kernel_heap;
+struct heap_table kernel_heap_table;
+
+void kheap_init()
+{
+    // heap size / block size
+    int total_table_entries = PEACHOS_HEAP_SIZE_BYTES / PEACHOS_HEAP_BLOCK_SIZE;
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*)(PEACHOS_HEAP_TABLE_ADDRESS);
+    kernel_heap_table.total = total_table_entries;
+
+    // calculate end variable (end of table)
+    void* end = (void*)(PEACHOS_HEAP_ADDRESS + PEACHOS_HEAP_SIZE_BYTES);
+    // create heap
+    int res = heap_create(&kernel_heap, (void*)(PEACHOS_HEAP_ADDRESS), end, &kernel_heap_table);
+    // if error
+    if(res < 0)
+    {
+        print("Failed to create heap\n");
+    }
+}
