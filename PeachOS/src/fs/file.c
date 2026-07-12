28e853ffa3a81d91a5b7894880a55cfbf95808fe
Robinhoets <robertets88@gmail.com>
2026-07-12 16:58:59 -0400

    init of filesystem

---
+++ PeachOS/src/fs/file.c
@@ -0,0 +1,149 @@
+#include "file.h"
+#include "config.h"
+#include "memory/memory.h"
+#include "status.h"
+#include "memory/heap/kheap.h"
+#include "kernel.h"
+
+struct filesystem* filesystems[PEACHOS_MAX_FILESYSTEMS];
+struct file_descriptor* file_descriptors[PEACHOS_MAX_FILE_DESCRIPTORS];
+
+/*
+    Purpose:
+    Return: Address of empty slot in array. 0 if no free filesystem.
+*/
+static struct filesystem** fs_get_free_filesystem()
+{
+    int i=0;
+    for(i=0; i<PEACHOS_MAX_FILESYSTEMS; i++)
+    {
+        if(filesystems[i] == 0)
+        {
+            return &filesystems[i];
+        }
+    }
+    
+    return 0;
+}
+
+/*
+    Purpose: To insert a filesystem.
+    Parameter filesystem: The filesystem to be inserted. 
+    Return: void.
+*/
+void fs_insert_filesystem(struct filesystem* filesystem)
+{
+    struct filesystem** fs;
+    fs = fs_get_free_filesystem();
+    if(!fs)
+    {
+        print("Problem inserting filesysem");
+        while(1){}
+    }
+    // Return element in array of free filesystem to address (*fs) provided.
+    *fs = filesystem;
+}
+
+/*
+    Purpose: Build filesystems into core kernel. Not dynamically loaded.
+*/
+static void fs_static_load()
+{
+    //fs_insert_filesystem(fat16_init());
+}
+
+/*
+    Purpose: Load all available filesystems.
+*/
+void fs_load()
+{
+    memset(filesystems, 0, sizeof(filesystems));
+    fs_static_load();
+}
+
+/*
+    Purpose: Initialize the filesystem.
+*/
+void fs_init()
+{
+    memset(file_descriptors, 0, sizeof(file_descriptors));
+    fs_load();
+}
+
+/*
+    Purpose: Create file descriptors.
+    Parameter desc_out: Location of descriptor in memory. Set to desc if free descriptor.
+    Return: -ENOMEM if no discriptors free. 0 if found descriptor.
+*/
+static int file_new_descriptor(struct file_descriptor** desc_out)
+{
+    int res = -ENOMEM;
+    for(int i=0; i<PEACHOS_MAX_FILE_DESCRIPTORS; i++)
+    {
+        // if file descriptor is free
+        if(file_descriptors[i] == 0)
+        {
+            struct file_descriptor* desc = kzalloc(sizeof(struct file_descriptor));
+            // Descriptors start at 1
+            desc->index = i + 1;
+            file_descriptors[i] = desc;
+            // pass descriptor back to caller
+            *desc_out = desc;
+            res = 0;
+            break;
+        }
+    }
+
+    return res;
+}
+
+/*
+    Purpose: Get file descriptor based on id.
+    Parameter fd:
+    Return:
+*/
+static struct file_descriptor* file_get_descriptor(int fd)
+{
+    if(fd <= 0 || fd >= PEACHOS_MAX_FILE_DESCRIPTORS)
+    {
+        return 0;
+    }
+
+    // Descriptors start at 1
+    int index = fd - 1;
+    return file_descriptors[index];
+}
+
+/*
+    Purpose: Get the memory location of the filesystem.
+    Parameter disk: Which disk filesystem on.
+    Return: Pointer to filesystem found.
+*/
+struct filesystem* fs_resolve(struct disk* disk)
+{
+    struct filesystem* fs = 0;
+    // Loop through all filesystems that were loaded using fs_insert_filesystem.
+    for(int i=0; i< PEACHOS_MAX_FILESYSTEMS; i++)
+    {
+        // Ensure filesystem there and not null pointer
+        // Then call internal resolve function
+        if(filesystems[i] != 0 && filesystems[i]->resolve(disk) == 0)
+        {
+            fs = filesystems[i];
+            break;
+        }
+    }
+
+    return fs;
+}
+
+/*
+    Purpose: 
+    Parameter filename:
+    Parameter mode:
+    Return
+*/
+int fopen(const char* filename, const char* mode)
+{
+    return -EIO;
+}
---
+++ PeachOS/src/fs/file.h
@@ -0,0 +1,57 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "pparser.h"
+
+// Seeking through a file
+typedef unsigned int FILE_SEEK_MODE;
+enum
+{
+    SEEK_SET,
+    SEEK_CUR,
+    SEEK_END
+}; 
+
+typedef unsigned int FILE_MODE;
+enum
+{
+    FILE_MODE_READ,
+    FILE_MODE_WRITE,
+    FILE_MODE_APPEND,
+    FILE_MODE_INVALID
+};
+
+struct disk;
+// function pointer
+typedef void*(*FS_OPEN_FUNCTION)(struct disk* disk, struct path_part* path, FILE_MODE mode);
+// function pointer for resolve pointer - is this valid disk
+typedef int (*FS_RESOLVE_FUNCTION)(struct disk* disk);
+
+struct filesystem
+{
+    // Filesystem should return zero from resolve if the provided disk is using its filesystem.
+    FS_RESOLVE_FUNCTION resolve;
+    FS_OPEN_FUNCTION open;
+
+    char name[20];
+};
+
+struct file_descriptor
+{
+    // The descriptor index
+    int index;
+    struct filesystem* filesystem;
+
+    // Private data for internal file descriptor
+    void* private;
+
+    // The disk that the file descriptor should be used on
+    struct disk* disk;
+};
+
+void fs_init();
+int fopen(const char* filename, const char* mode);
+void f_insert_filesystem(struct filesystem* filesystem);
+struct filesystem* fs_resolve(struct disk* disk);
+
+#endif
