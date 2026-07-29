328986d6a89992e4b2a90a9aa51f3c56b45aff39
Robinhoets <robertets88@gmail.com>
2026-07-29 12:54:06 -0400

    pathparser-c image readme

---
+++ PeachOS/src/fs/README.md
@@ -0,0 +1,8 @@
+# pathparser.c
+    Purpose: Take a file path and split it into drive number, directory and file
+    Parameter path: Full path, including drive, to parse.
+    Parameter current_directory_path: *** not currently used ***
+    Return: Path root.
+
+
+![pathparser.c](../imgs/pathparser-c.drawio.png)
--- PeachOS/src/fs/pparser.c
+++ PeachOS/src/fs/pparser.c
@@ -138,7 +138,7 @@ void pathparser_free(struct path_root* root)
 }
 
 /*
-    Purpose: Public function
+    Purpose: Take a file path and split it into drive number, directory and file
     Parameter path: Full path, including drive, to parse.
     Parameter current_directory_path: *** not currently used ***
     Return: Path root.
